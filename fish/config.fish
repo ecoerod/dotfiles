fdd8606cd896904b3d616e32b78b0c58e7f64297
Eduardo Coello Rodríguez <gkw5024@gmail.com>
2017-10-01 17:49:30 +0100

    Added Emacs to EDITOR properly

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,14 @@
 set -g -x CFLAGS "-O2"
 set -g -x MAKEFLAGS "-j"
 set -g -x BROWSER chromium
-set -g -x EDITOR emacs
+set -g -x EDITOR emacsclient -t
+set -g -x VISUAL emacsclient -t -a emacs
 set -g -x fish_greeting ""
 set -g -x PATH $PATH /home/nelthorim/.local/bin/
 
-# prompt
+# aliases
+alias emacs  "emacsclient -t"
+alias emacsw "emacsclient -c -a emacs"
 
 # pyenv settings
 set PYTHONPATH $PYTHONPATH $HOME/.python
