dad9dea83f4afcadcb2799234f6bf172bded2ff3
Eduardo Coello Rodríguez <gkw5024@gmail.com>
2018-04-12 14:27:24 +0100

    Disabled Doom packages

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -50,7 +50,7 @@
        make              ; run make tasks from Emacs
        magit             ;
        neotree           ; a project drawer, like NERDTree for vim
-       password-store    ; password manager for nerds
+      ;password-store    ; password manager for nerds
        pdf               ; pdf enhancements
        rgb               ; creating color strings
        rotate-text       ; cycle region at point between text candidates
@@ -59,29 +59,29 @@
        upload            ; map local to remote projects via ssh/ftp
 
        :lang
-       assembly          ; assembly for fun or debugging
-       cc                ; C/C++/Obj-C madness
-       crystal           ; ruby at the speed of c
+      ;assembly          ; assembly for fun or debugging
+      ;cc                ; C/C++/Obj-C madness
+      ;crystal           ; ruby at the speed of c
        clojure           ; java with a lisp
-       csharp            ; unity, .NET, and mono shenanigans
+      ;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
       ;erlang            ; an elegant language for a more civilized age
-       elixir            ; erlang done right
-       elm               ; care for a cup of TEA?
+      ;elixir            ; erlang done right
+      ;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
-       ess               ; emacs speaks statistics
-       go                ; the hipster dialect
+      ;ess               ; emacs speaks statistics
+      ;go                ; the hipster dialect
        (haskell +intero) ; a language that's lazier than I am
-       hy                ; readability of scheme w/ speed of python
-       (java +meghanada) ; the poster child for carpal tunnel syndrome
+      ;hy                ; readability of scheme w/ speed of python
+      ;(java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       julia             ; a better, faster MATLAB
+      ;julia             ; a better, faster MATLAB
        latex             ; writing papers in Emacs has never been so fun
-       ledger            ; an accounting system in Emacs
-       lua               ; one-based indices? one-based indices
+      ;ledger            ; an accounting system in Emacs
+      ;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        nix               ; I hereby declare "nix geht mehr!"
-       ocaml             ; an objective camel
+      ;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         +attach          ; custom attachment system
         +babel           ; running code in org
@@ -89,18 +89,18 @@
         +export          ; Exporting org to whatever you want
         +present         ; Emacs for presentations
         +publish)        ; Emacs+Org as a static site generator
-       perl              ; write code no one else can comprehend
-       php               ; perl's insecure younger brother
-       plantuml          ; diagrams for confusing people more
-       purescript        ; javascript, but functional
+      ;perl              ; write code no one else can comprehend
+      ;php               ; perl's insecure younger brother
+      ;plantuml          ; diagrams for confusing people more
+      ;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
-       rest              ; Emacs as a REST client
-       ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       scala             ; java, but good
+      ;rest              ; Emacs as a REST client
+      ;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+      ;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+      ;scala             ; java, but good
        sh                ; she sells (ba|z)sh shells on the C xor
-       swift             ; who asked for emoji variables?
-       typescript        ; javascript, but better
+      ;swift             ; who asked for emoji variables?
+      ;typescript        ; javascript, but better
        web               ; the tubes
 
        ;; Applications are complex and opinionated modules that transform Emacs
