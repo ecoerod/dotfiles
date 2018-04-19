16ccf298d957ad7068ffa346def95539e7f93680
Eduardo Coello Rodríguez <gkw5024@gmail.com>
2018-04-19 18:15:48 +0100

    Remove new workspace on frame creation + Spanish keyboard adjustments

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -9,3 +9,9 @@
   (local-set-key (kbd "<tab>") 'company-complete))
 
 (setq confirm-kill-emacs nil)
+
+(setq persp-interactive-init-frame-behaviour-override -1
+      persp-emacsclient-init-frame-behaviour-override -1)
+
+(define-key evil-motion-state-map "¡" 'evil-beginning-of-line)
+(define-key evil-motion-state-map "ç" 'evil-end-of-line)
