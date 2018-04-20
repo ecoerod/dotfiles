05fadc847f840d48709b1b5b6454e9820840493e
Eduardo Coello Rodríguez <gkw5024@gmail.com>
2018-04-20 14:44:05 +0100

    Changed vanilla Emacs commands to Doom convention

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -5,13 +5,14 @@
       doom-font
       (font-spec :name "Hack" :size 14))
 
-(add-hook! prog-mode
-  (local-set-key (kbd "<tab>") 'company-complete))
+(map! :mode prog-mode
+      :i "<tab>" 'company-complete)
+
+(map! :map evil-motion-state-map
+      "¡" 'evil-beginning-of-line
+      "ç" 'evil-end-of-line)
 
 (setq confirm-kill-emacs nil)
 
 (setq persp-interactive-init-frame-behaviour-override -1
       persp-emacsclient-init-frame-behaviour-override -1)
-
-(define-key evil-motion-state-map "¡" 'evil-beginning-of-line)
-(define-key evil-motion-state-map "ç" 'evil-end-of-line)
