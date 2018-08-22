49a61508f15f9dc77d526409bb58246e7ae4a812
Eduardo Coello Rodríguez <gkw5024@gmail.com>
2018-08-22 10:46:22 +0100

    Updated Doom Emacs

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -17,13 +17,13 @@
 
        :completion
        (company          ; the ultimate code completion backend
-        +auto
-        +childframe)
-      ;helm              ; the *other* search engine for love and life
+        +auto)
+       (helm             ; the *other* search engine for love and life
+        +fuzzy)
       ;ido               ; the other *other* search engine...
-       (ivy              ; a search engine for love and life
-        +fuzzy
-        +childframe)
+      ;(ivy              ; a search engine for love and life
+      ; +fuzzy
+      ; +childframe)
 
        :ui
        doom              ; what makes DOOM look the way it does
@@ -33,8 +33,9 @@
        evil-goggles      ; display visual hints when editing in evil
       ;fci
        hl-todo           ; highlight TODO/FIXME/NOTE tags
+       modeline
        nav-flash         ; blink the current line after jumping
-       neotree           ; a project drawer, like NERDTree for vim
+      ;neotree           ; a project drawer, like NERDTree for vim
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
@@ -46,6 +47,8 @@
        window-select     ; visually switch windows
 
        :editor
+      ;(format +onsave)
+       multiple-cursors
        parinfer
        rotate-text       ; cycle region at point between text candidates
 
@@ -67,15 +70,18 @@
        magit             ;
       ;password-store    ; password manager for nerds
        pdf               ; pdf enhancements
+      ;prodigy
        rgb               ; creating color strings
        tmux              ; an API for interacting with tmux
        upload            ; map local to remote projects via ssh/ftp
+      ;wakatime
 
        :lang
       ;assembly          ; assembly for fun or debugging
-      ;cc                ; C/C++/Obj-C madness
-      ;crystal           ; ruby at the speed of c
+      ;(cc +irony +tags) ; C/C++/Obj-C madness
       ;clojure           ; java with a lisp
+      ;common-lisp
+      ;crystal           ; ruby at the speed of c
       ;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
       ;erlang            ; an elegant language for a more civilized age
@@ -93,6 +99,7 @@
       ;ledger            ; an accounting system in Emacs
       ;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
+      ;nim
       ;nix               ; I hereby declare "nix geht mehr!"
       ;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
@@ -106,13 +113,15 @@
       ;plantuml          ; diagrams for confusing people more
       ;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
+      ;qt
+      ;racket
       ;rest              ; Emacs as a REST client
       ;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
       ;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
       ;scala             ; java, but good
-       sh                ; she sells (ba|z)sh shells on the C xor
+       (sh +fish)                ; she sells (ba|z)sh shells on the C xor
+      ;solidity
       ;swift             ; who asked for emoji variables?
-      ;typescript        ; javascript, but better
        web               ; the tubes
 
        ;; Applications are complex and opinionated modules that transform Emacs
